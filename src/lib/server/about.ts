53bbd2a2d2aab6f1a8700ec3f61ac837371b0612
Pedro de Azeredo Nogueira <pedro_azeredo@usp.br>
2026-06-06 17:54:00 +0200

    Change About default value (test)

--- src/lib/server/about.ts
+++ src/lib/server/about.ts
@@ -10,7 +10,7 @@ export interface AboutData {
 }
 
 const SEED: AboutData = {
-	bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+	bio: 'My name is Luane Catarina. I like to draw.',
 	photo: '/luane.jpeg'
 };
 
